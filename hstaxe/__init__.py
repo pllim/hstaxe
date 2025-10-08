ee7b5b003f9d7927b47470d20f1183594d7ef396
Larry Bradley <larry.bradley@gmail.com>
2025-10-08 15:53:21 -0400

    Replace deprecated pkg_resources

--- hstaxe/__init__.py
+++ hstaxe/__init__.py
@@ -2,7 +2,7 @@
 import the toplevel functions and check the directory setup
 See LICENSE.txt
 """
-from pkg_resources import get_distribution, DistributionNotFound
+from importlib.metadata import version, PackageNotFoundError
 
 from . import config
 from .axesrc import axetasks
@@ -13,9 +13,8 @@ config.check_axe_dirs()
 
 
 try:
-    release = get_distribution('hstaxe').version
-    __version__ = '.'.join(release.split('.')[:4])
-except DistributionNotFound:
+    __version__ = version('hstaxe')
+except PackageNotFoundError:
     # package is not installed
     __version__ = 'unknown'
     __githash__ = ''
